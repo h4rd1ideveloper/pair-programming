b6cdc010007c5a44793e7e066f3e3ab1a2f5c32c
yan <h4rd1i@gmail.com>
2025-06-27 10:08:11 -0300

    Add Jest configuration and setup for testing with React Testing Library

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type {Config} from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({dir: './'});
+
+const config: Config = {
+    clearMocks: true, // limpa mocks automaticamente
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    }
+};
+
+export default createJestConfig(config);
